e67e29abcb8a7c08ce11568f78fe86fe3a242b62
tiger <tiger.hu@ximalaya.com>
2017-12-02 20:00:03 +0800

    update config dynamic

--- bar-client/src/main/java/org/tiger/test/BarApplication.java
+++ bar-client/src/main/java/org/tiger/test/BarApplication.java
@@ -3,6 +3,7 @@ package org.tiger.test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class BarApplication {
 
     @Value("${config.name}")
